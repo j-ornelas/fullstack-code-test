e8a2fcc647f3f14992e17253680627e0aa848790
john ornelas <johnnyoh1988@gmail.com>
2019-07-10 17:29:43 -0700

    fix: change cron task from 2 minutes to 2 seconds

--- api/src/cron/scheduleLocationFetch.js
+++ api/src/cron/scheduleLocationFetch.js
@@ -2,5 +2,5 @@ import cron from 'node-cron';
 import { location } from '../api/v1/services';
 
 export default () => {
-  cron.schedule('*/2 * * * *', location.record);
+  cron.schedule('*/2 * * * * *', location.record);
 };
